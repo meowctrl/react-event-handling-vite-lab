4834d5b1f208994588bfbf586ec2959c68093411
carol <carolineallen1012@gmail.com>
2026-02-16 13:12:47 -0500

    all tests passing

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,13 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    function handleChange() {
+        console.log("Entering password...");
+    }
     return(
-        <></>
+        <>
+        Password: <input type="password" onChange={()=>handleChange()}></input>
+        </>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,16 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+    function handleEnter(){
+        console.log("Mouse Entering");
+    }
+    function handleLeave () {
+        console.log("Mouse Exiting");
+    }
     return(
-        <></>
+        <>
+        <button onMouseEnter={()=>handleEnter()} onMouseLeave={()=>handleLeave()}>Submit Password</button>
+        </>
     )
 }
 
